6947cf70e60f06eea3ffdec05e91ae891dd9c6a4
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-09 17:11:38 +0200

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Ostap Artym on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
