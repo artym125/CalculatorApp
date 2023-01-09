350dc849e2b68dcd4adf671fce3cfaadf7ae6d27
Ostap Artym <85745668+artym125@users.noreply.github.com>
2023-01-10 00:21:15 +0200

    equalitySingButton

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -8,12 +8,77 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var displayResultLabel: UILabel!
+    
+    var stillTyping = false
+    
+    var firstOperand: Double = 0
+    
+    var secondOperator: Double = 0
+    
+    var operationSign: String = ""
+    
+    var currentInput: Double {
+        get {
+            Double(displayResultLabel.text!)!
+        }
+        set {
+            displayResultLabel.text = "\(newValue)"
+            stillTyping = false
+        }
+    }
+    
+    
+    
+    
+    
+    //Action
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        
+        if stillTyping {
+            if displayResultLabel.text!.count < 20 {
+                displayResultLabel.text = displayResultLabel.text! + number}
+        } else {
+            displayResultLabel.text = number
+            stillTyping = true
+        }
     }
 
-
+    
+    @IBAction func TwoOperandsSignPressed(_ sender: UIButton) {
+        operationSign = sender.currentTitle!
+        firstOperand = currentInput
+        stillTyping = false
+        
+    }
+    
+    func operateWithTwoOperands(operation:(Double, Double) -> Double) {
+        currentInput = operation(firstOperand, secondOperator)
+        stillTyping = false
+    }
+    
+    
+    @IBAction func equalitySingButton(_ sender: UIButton) {
+        if stillTyping {
+            secondOperator = currentInput
+        }
+        
+        switch operationSign {
+        case "+":
+            operateWithTwoOperands{$0 + $1}
+        case "-":
+            operateWithTwoOperands{$0 - $1}
+        case "x":
+            operateWithTwoOperands{$0 * $1}
+        case "÷":
+            operateWithTwoOperands{$0 / $1}
+        default:
+            break
+        }
+    }
+    
+    
 }
-
